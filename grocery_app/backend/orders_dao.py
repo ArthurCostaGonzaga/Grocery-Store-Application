ca80689e9d29d30bd9d6b4e2902eb5f48f0d6496
Arthur Gonzaga <arthurgonz@gmail.com>
2023-03-08 21:42:25 +0200

    first modifications in the orders.dao

--- grocery_app/backend/orders_dao.py
+++ grocery_app/backend/orders_dao.py
@@ -1,2 +1,24 @@
 
 
+def insert_order(connection, order):
+    cursor = connection.cursor()
+
+if __name__ == '__name__':
+    connection = get_sql_connection()
+    print(insert_order(connection, {
+        'costumer_name': 'dhaval',
+        'grand_total': '500,'
+        'datetime': datetime.now(),
+        'order_details': [
+            {
+                'product_id': 1,
+                'quantity': 2,
+                'total_price':50
+            },
+            {
+                'product_id': 3,
+                'quantity': 1,
+                'total_price':30
+            },
+        ]
+    }))
